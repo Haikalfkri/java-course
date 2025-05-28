b5f9dee6ee61795e65256e9889b4b379e8352646
Haikal Fikri <haikaljr1245@gmail.com>
2025-05-29 05:39:49 +0700

    First Java Program

---
+++ Main/Main.java
@@ -0,0 +1,18 @@
+package Main;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // this is java program
+
+        /*
+        * this is multi-line comment
+        * */
+
+        System.out.println("I Like Pizza");
+        System.out.println("It's Very Good");
+
+    }
+
+}
